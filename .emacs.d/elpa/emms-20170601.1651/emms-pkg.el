0ccd192875f82dfa7783bc1d5ad3d71cb103e031
Tim Ransom <ransomtim8078@gmail.com>
2017-06-11 10:26:53 -0400

    Add emacs elpa libraries

---
+++ .emacs.d/elpa/emms-20170601.1651/emms-pkg.el
@@ -0,0 +1,5 @@
+(define-package "emms" "20170601.1651" "The Emacs Multimedia System" 'nil :keywords
+  '("emms" "mp3" "mpeg" "multimedia"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ .emacs.d/elpa/emms-info-mediainfo-20131223.500/emms-info-mediainfo-autoloads.el
@@ -0,0 +1,16 @@
+;;; emms-info-mediainfo-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("emms-info-mediainfo.el") (22844 46225
+;;;;;;  341810 677000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; emms-info-mediainfo-autoloads.el ends here
